05f609cbcf54c74de4d38e77efef39a6cf1c3165
Syamsul Huda Harisul Muslimin <syamsulhuda748@gmail.com>
2024-02-09 15:22:55 +0700

    commit pertama

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Redux from './Redux'
+
+function App() {
+
+  return (
+    <>
+      <Redux />
+    </>
+  )
+}
+
+export default App
---
+++ src/Redux/pembahasan/counter.jsx
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrementWithCheckingAction, increment } from "../../app/Features/Counter/actions";
+
+const Counter = () => {
+
+    let {count} = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
+    return (
+      <div>
+        <button onClick={() => dispatch(decrementWithCheckingAction(1))}>-</button>
+        <span> {count} </span>
+        <button onClick={() => dispatch(increment(1))}>+</button>
+      </div>
+    );
+  };
+  
+  export default Counter;
+  
